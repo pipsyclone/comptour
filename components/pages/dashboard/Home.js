6c3f7d40e0f137edb5cf999c21fd2015a7ff2a6f
unknown <apipska.10@gmail.com>
2024-05-12 16:09:10 +0700

    Error Build Fixed

--- components/pages/dashboard/Home.js
+++ components/pages/dashboard/Home.js
@@ -63,17 +63,20 @@ export default function HomeComponent() {
                         myLocation && <MapContainer center={myLocation} zoom={5} scrollWheelZoom={true} style={{ width: '100%', height: '500px' }}>
                             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                             {
-                                data.map((data, key) => {
-                                    return (
-                                        <Marker position={[data?.longtitude, data?.latitude]} icon={wisataMarker}>
-                                            <Popup>
-                                                <h3>{data.name_place}</h3>
-                                                {data.description}
-                                            </Popup>
-                                            <Tooltip>{data.name_place}</Tooltip>
-                                        </Marker>
-                                    )
-                                })
+                                data.length < 1 ?
+                                    ''
+                                    :
+                                    data.map((data, key) => {
+                                        return (
+                                            <Marker position={[data?.longtitude, data?.latitude]} icon={wisataMarker}>
+                                                <Popup>
+                                                    <h3>{data.name_place}</h3>
+                                                    {data.description}
+                                                </Popup>
+                                                <Tooltip>{data.name_place}</Tooltip>
+                                            </Marker>
+                                        )
+                                    })
                             }
                             {
                                 myLocation &&
